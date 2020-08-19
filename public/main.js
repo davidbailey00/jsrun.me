1e2823d215f45d66bd8841e1826cf89625d69d33
David Bailey <davidbailey00@outlook.com>
2020-08-19 19:38:11 +0100

    initial commit

---
+++ public/main.js
@@ -0,0 +1,21 @@
+function createDocument(js) {
+  const html = String.raw;
+  const doc = html`<html>
+    <body>
+      <script>
+        eval(unescape('${escape(js)}'));
+      </script>
+    </body>
+  </html>`;
+
+  return '<!DOCTYPE html>' + doc;
+}
+
+if (location.hash !== '') {
+  const js = decodeURIComponent(location.hash.substring(1));
+  const doc = createDocument(js);
+
+  const iframe = document.createElement('iframe');
+  iframe.src = 'data:text/html,' + encodeURI(doc);
+  document.body.appendChild(iframe);
+}
